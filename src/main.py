6e6c0977acded3436c1315e4ab30a4e77f91d589
REIMEI <kakibo1213@gmail.com>
2020-08-24 23:41:08 +0900

    add

--- src/main.py
+++ src/main.py
@@ -16,10 +16,7 @@ class Application(tk.Frame):
 
         self.product_list_path = '../product_list/'
         self.files = os.listdir(self.product_list_path)
-        #self.product_count = len(self.files)
         self.product_count = len(fnmatch.filter(os.listdir(self.product_list_path), '*.txt'))
-
-        print(self.product_count)
 
         #要素の追加・削除ボタンの配置
         self.baseControlleFrame = tk.Frame(master, width=1000, height=50,borderwidth=1, relief='groove')  #基本操作ボタンをまとめるフレーム
@@ -29,48 +26,37 @@ class Application(tk.Frame):
         self.delButton.pack(fill='x', padx=20, side='left')
 
         #プロダクト一覧の配置
-        self.productListFrame = tk.Frame(master, width=250, borderwidth=1, relief='groove')
+        self.productList_baseFrame = tk.Frame(master, width=250)
+        self.canvas = tk.Canvas(self.productList_baseFrame, width=250)
+        self.List_bar = tk.Scrollbar(self.productList_baseFrame, orient=tk.VERTICAL)
+        self.List_bar.pack(side='right', fill='y')
+        self.List_bar.config(command=self.canvas.yview)
+        self.canvas.config(yscrollcommand=self.List_bar.set)
+        self.canvas.config(scrollregion=(0, 0, 250, self.product_count*60))
+        self.productListFrame = tk.Frame(self.canvas, borderwidth=1, relief='groove')
+        self.canvas.create_window((0, 0), window=self.productListFrame, anchor=tk.NW, width=self.canvas.cget('width'))
+
         self.productList = []
         for i in range(0, self.product_count):
             #self.product_element = tk.Button(self.productListFrame, text='aa', width=25, height=3, command=self.buttonPressed)
-            self.productList.append(tk.Button(self.productListFrame, text='aa', width=25, height=3, command=self.buttonPressed))
+            self.productList.append(tk.Button(self.productListFrame, text='Product', width=25, height=3, command=self.buttonPressed))
             self.productList[i].pack()
 
 
-        '''''
-        self.Button = tk.Button(self.productListFrame, text='aa', width=25, height=3, command=self.buttonPressed)
-        #self.Button.pack()
-        self.Button2 = tk.Button(self.productListFrame, text='bb', width=25, height=3)
-        #self.Button2.pack()
-        self.Button_list.append(self.Button)
-        self.Button_list.append(self.Button2)
-        self.Button_list[0].pack()
-        self.Button_list[1].pack()
-        '''''
-
-        #self.txtBox = tk.Entry()
-        # txtBox.configure(state='normal', width=50, height=30)
-        #self.txtBox.place(x=500, y=10, width=500, height=100)
-
-        #self.button = tk.Button(text='ボタン', width=30, command=self.outputWords)
-        #self.button.pack(anchor='w')
-
         self.baseControlleFrame.pack(fill='x', padx=10, pady=10, ipady=5)
-        self.productListFrame.pack(fill='y', side='left', padx=10, pady=10, ipadx=5, ipady=5)
-
-    #def outputWords(self):
-        #self.txtBox.insert(tk.END, "Hello!!")
+        self.canvas.pack(fill='y', side='left')
+        self.productList_baseFrame.pack(fill='y', side='left')
 
     def buttonPressed(self):
         self.Button.config(bg='#FF0000')
 
     def addProduct(self):
         product_path = pathlib.Path(self.product_list_path + 'test' + str(self.product_count+1) + '.txt')
-        #product_path = pathlib.Path('../product_list/test.txt')
         product_path.touch()
         self.product_count += 1
         self.productList.append(tk.Button(self.productListFrame, text='aa', width=25, height=3, command=self.buttonPressed))
         self.productList[self.product_count-1].pack()
+        self.canvas.config(scrollregion=(0, 0, 250, self.product_count * 60))
 
 def main():
     root = tk.Tk()
@@ -79,3 +65,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
